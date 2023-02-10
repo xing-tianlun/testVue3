76952c521976085170300747ee90f28a26f22470
xingtianlun <zhongxingtianlun@163.com>
2023-02-11 01:05:47 +0800

    first commit

---
+++ src/views/Ab.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ obj.num }}</div>
+  <button @click="btn">btn</button>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+let num = ref(1);
+let obj = reactive({
+  num: 10,
+});
+function btn() {
+  obj.num++;
+}
+</script>
+<style lang="scss" scoped></style>
